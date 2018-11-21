070916f0b25e995206477087939d07cc05f5ec52
RocketBear27 <rocketbear27.code@gmail.com>
2018-11-21 10:56:04 -0800

    Search Bar

--- README.md
+++ README.md
@@ -1,10 +1,2 @@
 # google.com
 A replica of google.com!
-
-
-## Features to Add or Fix
-1. Fix the Sign in button styles
-2. Add footer
-3. Add search engine bar
-4. Add logo
-5. Add Botton buttons
--- src/script.js
+++ src/script.js
@@ -1,1 +1,7 @@
-//
+var input = document.getElementById("search").value();
+input.addEventListener("keyup", function (event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+        window.open("https://www.google.com/search?q=" + input + "&rlz=1C1GTPM_enUS820US820&oq=" + input + "&aqs=chrome..69i57j69i60l2j69i61j69i60j35i39.1143j0j7&sourceid=chrome&ie=UTF-8");
+    }
+});
