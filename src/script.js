7e3821591a1796922ebbac52ea3a08003effa186
Caleb H <42253012+naclcaleb@users.noreply.github.com>
2018-12-20 07:46:32 -0800

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,10 +1,10 @@
 var input = document.getElementById("search");
 input.addEventListener("keyup", function (event) {
     event.preventDefault();
-   /* if (event.keyCode === 13) {
-        window.open("https://www.google.com/search?q=" + input + "&rlz=1C1GTPM_enUS820US820&oq=" + input + "&aqs=chrome..69i57j69i60l2j69i61j69i60j35i39.1143j0j7&sourceid=chrome&ie=UTF-8");
-    }*/
-    queryGoogle(input.value);
+    if (event.keyCode === 13) {
+        //window.open("https://www.google.com/search?q=" + input + "&rlz=1C1GTPM_enUS820US820&oq=" + input + "&aqs=chrome..69i57j69i60l2j69i61j69i60j35i39.1143j0j7&sourceid=chrome&ie=UTF-8");
+        queryGoogle(input.value);
+    }
 });
 
 
